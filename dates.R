df0a38a4e539e6067fa805040ab66fd158943d7d
Stanislav Bashkyrtsev <ctapobep@javatalks.ru>
2014-10-25 13:00:34 +0400

    date sequencing

--- dates.R
+++ dates.R
@@ -6,6 +6,10 @@ d2 = as.Date('2012-01-01')
 
 assert_that(
   d2 - d1 == 365,
-  are_equal(as.integer(difftime(d2, d1, units = 'weeks')), 52),
+  are_equal(as.integer(difftime(d2, d1, units = 'weeks')), 51),
   are_equal(as.integer(difftime(d2, d1, units = 'days')), 365),
   are_equal(as.integer(difftime(d2, d1, units = 'days')), 365))
+
+assert_that(
+  are_equal(seq(d1, length = 2, by = "week"), as.Date(c('2011-01-01', '2011-01-08'))))
+
