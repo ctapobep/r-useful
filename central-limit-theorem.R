39574ab748080f81d973eab5993c764517a7e3ea
Stanislav Bashkyrtsev <ctapobep@javatalks.ru>
2014-12-21 15:58:00 +0300

    Central Limit Theorem

--- central-limit-theorem.R
+++ central-limit-theorem.R
@@ -1,12 +1,19 @@
 if (!require('ggplot2')) { install.packages('ggplot2'); library('ggplot2') }
-#### NORMAL DISTRIBUTION ####
-
-population=with(density(sample(rep(1:100), replace = TRUE, size = 500), bw = 'SJ'), data.frame(x,y))
-ggplot(data = population, aes(x=x, y=y)) + geom_line()
+# When we take samples from population and calculate the mean - this mean imperfectly describes
+# the population mean. Hence, when presenting the numbers, we cannot tell a single number -
+# we should mention the range of possible values. Cenral limit theorem helps with this.
+population=with(density(sample(rep(1000:5000), replace = TRUE, size = 500), bw = 'SJ'), data.frame(x,y))
+ggplot(data = population, aes(x=x, y=y)) + geom_line() +
+  geom_vline(x = mean(population$x), color = 'red')
 samples = c()
-samples = c(samples,mean(sample(population$x, 50)))
+
+samples = c(samples, mean(sample(population$x, 50)))
 sampleDensity = with(density(samples, bw='SJ'), data.frame(x,y))
 qplot(samples, width=0.5, binwidth=1) + geom_vline(x=mean(samples), color='red', alpha=0.5) +
-  geom_line(data=sampleDensity, aes(x=x,y=y*length(samples)), color ='red', alpha=0.7)
+  scale_y_continuous(limits = c(0, max(sampleDensity$y)*length(samples))) +
+  geom_vline(x=mean(population$x), color = 'red', linetype=1, size=1) +
+  geom_vline(x=tail(samples, 1)) +
+  geom_line(data=sampleDensity, aes(x=x,y=y*length(samples)), color ='steelblue', alpha=0.7)
+
 sd(samples) #standard error
 sd(sample(population$x, 50))/sqrt(50) #standard error
